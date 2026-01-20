5993a81cd04940c520df623ba2a2ce353aacc511
Monkey <zhoub1001@126.com>
2026-01-20 16:04:32 +0800

    0900 done

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: "zhitalk-agent",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: process.env.PORT || 3000,
+      },
+      error_file: "./logs/pm2-error.log",
+      out_file: "./logs/pm2-out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: "1G",
+    },
+  ],
+};
